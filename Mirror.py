0343f53f8e17afd6fae9245d6c71cf9480d94597
JD <johndavid.anthony1@gmail.com>
2019-12-05 19:32:55 -0500

    Layed groundwork for forecast - Currently showing next 3 hours

--- Mirror.py
+++ Mirror.py
@@ -15,35 +15,6 @@ import spotipy.util as util
 from utilities import *
 
 
-class DynamicLabel(tk.Label):
-    def __init__(self, *args, **kwargs):
-        tk.Label.__init__(self, *args, **kwargs)
-
-        # clone the font, so we can dynamically change
-        # it to fit the label width
-        font = self.cget("font")
-        base_font = tk.font.nametofont(self.cget("font"))
-        self.font = tk.font.Font()
-        self.font.configure(**base_font.configure())
-        self.configure(font=self.font)
-
-        self.bind("<Configure>", self._on_configure)
-
-    def _on_configure(self, event):
-        text = self.cget("text")
-
-        # first, grow the font until the text is too big,
-        size = self.font.actual("size")
-        while size < event.width:
-            size += 1
-            self.font.configure(size=size)
-
-        # ... then shrink it until it fits
-        while size > 1 and self.font.measure(text) > event.width:
-            size -= 1
-            self.font.configure(size=size)
-
-
 class Clock:
     def __init__(self, parent, anchor="s", side="bottom"):
         self.time = None
@@ -57,24 +28,14 @@ class Clock:
         time = strftime("%H:%M:%S %p")
         self.time_label.config(text=time)
         self.time_label.after(1000, self.tick)
-    #
-    # def resize(self, event):
-    #     text = self.time_label.cget("text")
-    #     size = self.font.actual("size")
-    #     while size < event.width - 5:
-    #         size += 1
-    #         self.font.configure(size=size)
-    #
-    #     # ... then shrink it until it fits
-    #     while size > 1 and self.font.measure(text) > event.width:
-    #         size -= 1
-    #         self.font.configure(size=size)
 
 
 class Weather:
-    def __init__(self, parent, address, anchor="w", side="left"):
+    def __init__(self, parent, geo_location, anchor="w", side="left"):
         self.frame = tk.Frame(parent, bg="black", borderwidth=1)
         self.frame.pack(expand=True, fill="both", side=side, anchor=anchor)
+
+        self.geo_location = geo_location
 
         # Current Weather
 
@@ -100,18 +61,45 @@ class Weather:
         self.current_weather_label.pack(anchor=anchor, side=side, expand=True, fill="x", padx=5)
         self.current_weather_label.config(text="                             ")
 
+        # Future Weather _________________
 
+        self.future_weather_frame = tk.Frame(self.frame, bg="black", borderwidth=1)
+        self.future_weather_frame.pack(side="top", anchor="n", fill="x", expand=True)
+
+        # 3hr Weather icon
+        self.hr3_frame = tk.Frame(self.frame, bg="black", borderwidth=1)
+        self.hr3_frame.pack(side="top", anchor="n", fill="x", expand=True)
+        self.hr3_weather_canvas = tk.Canvas(self.future_weather_frame, bg="black", width=75, height=75,
+                                                highlightthickness=0)
+        self.hr3_weather_canvas.grid(columnspan=3, row=1, padx=50)
+        self.hr3_weather_image = ImageTk.PhotoImage(file="assets/cloudy.png")
+        self.hr3_weather_canvas.update()
+        x = self.hr3_weather_canvas.winfo_width() / 2
+        y = self.hr3_weather_canvas.winfo_height() / 2
+        self.hr3_image_holder = self.hr3_weather_canvas.create_image(x, y, anchor="center",
+                                                                     image=self.hr3_weather_image)
+        self.hr3_title = tk.Label(self.future_weather_frame, font=('Helvetica', FONTSIZE),
+                                               fg="white",
+                                               bg="black", text="Later Today")
+        self.hr3_title.grid(row=0, columnspan=3, padx=5)
+        self.hr3_weather_temp_label = tk.Label(self.future_weather_frame, font=('Helvetica', int(FONTSIZE/2)), fg="white",
+                                                   bg="black")
+        self.hr3_weather_temp_label.grid(row=2, column=2, padx=5)
+        self.hr3_weather_label = tk.Label(self.future_weather_frame, font=('Helvetica', int(FONTSIZE/2)), fg="white",
+                                              bg="black")
+        self.hr3_weather_label.grid(row=2, columnspan=2, padx=5)
 
         self.weather_description = ""
         self.weather_id = ""
         self.weather_temp_current = ""
-        self.address = address
 
-        self.frame.after(0, self.getWeatherData)
+        self.frame.after(0, self.getCurrentWeather)
+        self.frame.after(0, self.getFutureWeather)
 
-    def getWeatherData(self):
-        print("GetWeatherRun")
-        with urllib.request.urlopen(self.address) as url:
+    def getCurrentWeather(self):
+        print("GetCurrentWeatherRun")
+        address = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&APPID={}&units=metric".format(self.geo_location[0], self.geo_location[1], WEATHER_APIKEY)
+        with urllib.request.urlopen(address) as url:
             data = json.loads(url.read().decode())
             weather_description = data["weather"][0]["description"]
             weather_id = data["weather"][0]["id"]
@@ -120,53 +108,76 @@ class Weather:
             self.current_weather_label.config(text=weather_description)
             self.current_weather_temp_label.config(text=str(round((weather_temp_current*2)/2)) + "°C")
 
-            # Find right image for weather id
-            if weather_id > 803:
-                img_path = "assets/cloudy.png"
-            elif weather_id > 802:
-                img_path = "assets/cloudy_mostly.png"
-            elif weather_id > 801:
-                img_path = "assets/cloudy_partly.png"
-            elif weather_id > 801:
-                img_path = "assets/sunny_mostly.png"
-            elif weather_id > 781:
-                img_path = "assets/sunny.png"
-            elif weather_id > 622:
-                img_path = "assets/cloudy.png"
-            elif weather_id > 602:
-                img_path = "assets/sleet.png"
-            elif weather_id > 601:
-                img_path = "assets/snow_heavy.png"
-            elif weather_id > 600:
-                img_path = "assets/snow_medium.png"
-            elif weather_id > 531:
-                img_path = "assets/snow_light.png"
-            elif weather_id > 511:
-                img_path = "assets/rain_medium.png"
-            elif weather_id > 504:
-                img_path = "assets/sleet.png"
-            elif weather_id > 501:
-                img_path = "assets/rain_heavy.png"
-            elif weather_id > 500:
-                img_path = "assets/rain_medium.png"
-            elif weather_id > 231:
-                img_path = "assets/rain_light.png"
-            elif weather_id >= 200:
-                img_path = "assets/thunder.png"
-            else:
-                img_path = "assets/cloudy.png"
-
+            img_path = self.code_to_path(weather_id)
             self.current_weather_image = ImageTk.PhotoImage(PIL.Image.open(img_path))
-            x = self.current_weather_canvas.winfo_width() / 2
-            y = self.current_weather_canvas.winfo_height() / 2
 
             self.current_weather_canvas.itemconfig(self.image_holder, image=self.current_weather_image)
-            #self.current_weather_canvas.create_image(75, 75, anchor="center", image=self.current_weather_image) delete this
+
+        self.frame.after(3600000, self.getCurrentWeather)
+
+    def getFutureWeather(self):
+        print("GetFutureWeatherRun")
+        address = "http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&APPID={}&units=metric".format(
+            self.geo_location[0], self.geo_location[1], WEATHER_APIKEY)
+        with urllib.request.urlopen(address) as url:
+            data = json.loads(url.read().decode())
+
+            hours_3 = data["list"][0]
+            img_path = self.code_to_path(hours_3["weather"][0]["id"])
+            image = Image.open(img_path)
+            image = image.resize((75, 75), Image.ANTIALIAS)
+            self.hr3_weather_image = ImageTk.PhotoImage(image)
+            self.hr3_weather_canvas.itemconfig(self.hr3_image_holder, image=self.hr3_weather_image)
+            self.hr3_weather_temp_label.config(text=str(round((hours_3["main"]["temp"] * 2) / 2)) + "°C")
+            self.hr3_weather_label.config(text=hours_3["weather"][0]["description"])
+
+            day_1 = data["list"][3]
+            day_2 = data["list"][7]
+            day_3 = data["list"][11]
 
 
-        self.frame.after(3600000, self.getWeatherData)
 
 
+        self.frame.after(10800000, self.getFutureWeather)
+
+    def code_to_path(self, code):
+        # Find right image for weather id
+        if code > 803:
+            img_path = "assets/cloudy.png"
+        elif code > 802:
+            img_path = "assets/cloudy_mostly.png"
+        elif code > 801:
+            img_path = "assets/cloudy_partly.png"
+        elif code > 801:
+            img_path = "assets/sunny_mostly.png"
+        elif code > 781:
+            img_path = "assets/sunny.png"
+        elif code > 622:
+            img_path = "assets/cloudy.png"
+        elif code > 602:
+            img_path = "assets/sleet.png"
+        elif code > 601:
+            img_path = "assets/snow_heavy.png"
+        elif code > 600:
+            img_path = "assets/snow_medium.png"
+        elif code > 531:
+            img_path = "assets/snow_light.png"
+        elif code > 511:
+            img_path = "assets/rain_medium.png"
+        elif code > 504:
+            img_path = "assets/sleet.png"
+        elif code > 501:
+            img_path = "assets/rain_heavy.png"
+        elif code > 500:
+            img_path = "assets/rain_medium.png"
+        elif code > 231:
+            img_path = "assets/rain_light.png"
+        elif code >= 200:
+            img_path = "assets/thunder.png"
+        else:
+            img_path = "assets/cloudy.png"
+
+        return img_path
 
     def resize(self, event):
         text = self.current_weather_label.cget("text")
@@ -208,7 +219,7 @@ class Spot:
         self.default_cover_art = "https://lh3.googleusercontent.com/UrY7BAZ-XfXGpfkeWg0zCCeo-7ras4DCoRalC_WXXWTK9q5b0Iw7B0YQMsVxZaNB7DM"
         image_byt = urllib.request.urlopen(self.default_cover_art).read()
         img = PIL.Image.open(io.BytesIO(image_byt))
-        img = img.resize((150, 150), PIL.Image.ANTIALIAS)  # The (250, 250) is (height, width)
+        img = img.resize((150, 150), PIL.Image.ANTIALIAS)
         self.photo = ImageTk.PhotoImage(img)
 
         self.current_song_canvas.update()
@@ -319,9 +330,7 @@ class FullScreen:
 
 
         # Weather
-        self.weather = Weather(self.leftFrame,
-                               "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&APPID={}&units=metric".format(
-                                   geolocation[0], geolocation[1], WEATHER_APIKEY), "e", "right")
+        self.weather = Weather(self.leftFrame, geolocation, "e", "right")
         self.spotify = Spot(self.rightFrame, "n", "top")
 
 
